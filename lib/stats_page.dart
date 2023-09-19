b4eb4f613d605199aaed347c6846506a5b9a2c7e
bepnos_ <bepnos@proton.me>
2023-09-19 16:59:45 +0200

    Stats page graph started

--- lib/stats_page.dart
+++ lib/stats_page.dart
@@ -2,6 +2,7 @@ import 'package:provider/provider.dart';
 import 'package:focuser/study_data.dart';
 
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StatsPage extends StatelessWidget {
   const StatsPage({
@@ -11,34 +12,77 @@ class StatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var studyData = Provider.of<StudyData>(context);
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text("Stats"),
-          Text("Daily: ${studyData.getTimeDaily()}"),
-          Text("Weekly: ${studyData.getTimeWeekly()}"),
-          Text("All-time: ${studyData.getTimeAll()}"),
-          const SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              studyData.deleteStats();
-            },
-            child: const Text("Reset"),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                studyData.testMethod();
-              },
-              child: const Text("Test")),
-          ElevatedButton(
-              onPressed: () {
-                studyData.testMethod2();
-              },
-              child: const Text("Test2"))
-        ],
+    return SafeArea(
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("Time: ${studyData.getTime()}"),
+                ),
+                // space that takes up the rest of the space
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: DropdownMenu(
+                    dropdownMenuEntries: studyData.getDropdownMenuEntries(),
+                    onSelected: (index) {
+                      studyData.dropdownChanged(index);
+                    },
+                    initialSelection: 0,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            SfCartesianChart(
+              plotAreaBorderWidth: 0,
+              title: ChartTitle(
+                text: "${studyData.getTimeWeekly()}",
+              ),
+              primaryXAxis: CategoryAxis(
+                majorGridLines: const MajorGridLines(width: 0),
+              ),
+              primaryYAxis: NumericAxis(
+                  axisLine: const AxisLine(width: 0),
+                  labelFormat: '{value}',
+                  majorTickLines: const MajorTickLines(size: 0)),
+              series: studyData.getWeeklySeries(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      studyData.deleteStats();
+                    },
+                    child: const Text("Reset"),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        studyData.testMethod();
+                      },
+                      child: const Text("Test")),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        studyData.testMethod2();
+                      },
+                      child: const Text("Test2")),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/study_data.dart
+++ lib/study_data.dart
@@ -2,6 +2,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import "dart:async";
 import "dart:convert";
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class StudyData extends ChangeNotifier {
   late SharedPreferences prefs;
@@ -9,18 +10,42 @@ class StudyData extends ChangeNotifier {
   late Timer internalTimer;
   bool counting = false;
   int pageIndex = 0;
+  int currentSelected = 0;
 
   // general format of stats:
   // [
-  //  { "date": "2021-09-01", "time": 1234 },
+  //  { "date": "2021-09-01", "time": 1234 "project": "App-Development"},
   // ]
   List<Map<String, dynamic>> stats = [];
+  List<String> projects = [];
 
   Stopwatch get getStopwatch => stopwatch;
   SharedPreferences get getPrefs => prefs;
+  List<String> get getProjects => projects;
 
   StudyData() {
     init();
+  }
+
+  List<DropdownMenuEntry> getDropdownMenuEntries() {
+    return [
+      const DropdownMenuEntry(
+        value: 0,
+        label: "Daily",
+      ),
+      const DropdownMenuEntry(
+        value: 1,
+        label: "Weekly",
+      ),
+      const DropdownMenuEntry(
+        value: 2,
+        label: "Monthly",
+      ),
+      const DropdownMenuEntry(
+        value: 3,
+        label: "All",
+      ),
+    ];
   }
 
   void init() {
@@ -31,17 +56,75 @@ class StudyData extends ChangeNotifier {
     });
   }
 
+  bool addProject(String project) {
+    if (projects.contains(project)) {
+      return false;
+    }
+    projects.add(project);
+    prefs.setStringList("projects", projects);
+    notifyListeners();
+    return true;
+  }
+
+  void loadProjects() {
+    List<String>? projects = prefs.getStringList("projects");
+    if (projects != null) {
+      this.projects = projects;
+    }
+  }
+
+  void removeProject(String project) {
+    projects.remove(project);
+    prefs.setStringList("projects", projects);
+    notifyListeners();
+  }
+
   void testMethod() {
-    print("$stats");
-    Map<String, dynamic> newSession = {"date": "2023-09-01", "time": 1234};
+    Map<String, dynamic> newSession = {
+      "date": "2023-09-19",
+      "time": 29000,
+      "project": "App-Development"
+    };
     stats.add(newSession);
+    newSession = {
+      "date": "2023-09-18",
+      "time": 26000,
+      "project": "App-Development"
+    };
+    stats.add(newSession);
+    newSession = {
+      "date": "2023-09-17",
+      "time": 23000,
+      "project": "App-Development"
+    };
+    stats.add(newSession);
+    newSession = {
+      "date": "2023-09-16",
+      "time": 24000,
+      "project": "App-Development"
+    };
+    stats.add(newSession);
+    newSession = {
+      "date": "2023-09-14",
+      "time": 31000,
+      "project": "App-Development"
+    };
+    stats.add(newSession);
+
+    prefs.setString("stats", jsonEncode(stats));
     notifyListeners();
   }
 
   void testMethod2() {
-    Map<String, dynamic> newSession = {"date": "2023-08-01", "time": 2000};
-    stats.add(newSession);
-    notifyListeners();
+    aggregatedWeeklyData();
+  }
+
+  Map<String, int> testGraph() {
+    Map<String, int> weeklyData = {};
+    weeklyData["2021-09-01"] = 100;
+    weeklyData["2021-09-02"] = 200;
+    weeklyData["2021-09-03"] = 300;
+    return weeklyData;
   }
 
   void loadStats() {
@@ -95,15 +178,36 @@ class StudyData extends ChangeNotifier {
 
   void deleteStats() {
     stats = [];
+    projects = [];
+    prefs.setStringList("projects", projects);
     prefs.setString("stats", jsonEncode(stats));
     notifyListeners();
   }
 
-  String getTimeDaily() {
-    String today = DateTime.now().toString().substring(0, 10);
+  void dropdownChanged(int selected) {
+    currentSelected = selected;
+    notifyListeners();
+  }
+
+  String getTime() {
+    switch (currentSelected) {
+      case 0:
+        return getTimeDaily();
+      case 1:
+        return getTimeWeekly();
+      case 2:
+        return getTimeMonthly();
+      case 3:
+        return getTimeAll();
+      default:
+        return getTimeDaily();
+    }
+  }
+
+  String getTimeSpecificDate(String date) {
     int totalSeconds = 0;
     for (int i = 0; i < stats.length; i++) {
-      if (stats[i]["date"] == today) {
+      if (stats[i]["date"] == date) {
         totalSeconds += int.parse(stats[i]["time"].toString());
       }
     }
@@ -111,6 +215,11 @@ class StudyData extends ChangeNotifier {
     int minutes = (totalSeconds % 3600) ~/ 60;
     int seconds = totalSeconds % 60;
     return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
+  }
+
+  String getTimeDaily() {
+    String today = DateTime.now().toString().substring(0, 10);
+    return getTimeSpecificDate(today);
   }
 
   String getTimeWeekly() {
@@ -130,6 +239,24 @@ class StudyData extends ChangeNotifier {
     return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
 
+  String getTimeMonthly() {
+    DateTime today = DateTime.now();
+    DateTime lastMonth = today.subtract(const Duration(days: 30));
+    int totalSeconds = 0;
+
+    for (int i = 0; i < stats.length; i++) {
+      DateTime date = DateTime.parse(stats[i]["date"]);
+      if (date.isAfter(lastMonth)) {
+        totalSeconds += int.parse(stats[i]["time"].toString());
+      }
+    }
+
+    int hours = totalSeconds ~/ 3600;
+    int minutes = (totalSeconds % 3600) ~/ 60;
+    int seconds = totalSeconds % 60;
+    return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
+  }
+
   String getTimeAll() {
     int totalSeconds = 0;
     for (int i = 0; i < stats.length; i++) {
@@ -140,4 +267,67 @@ class StudyData extends ChangeNotifier {
     int seconds = totalSeconds % 60;
     return "${hours.toString().padLeft(2, '0')}:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}";
   }
+
+  Map<String, double> aggregatedWeeklyData() {
+    DateTime today = DateTime.now();
+    DateTime lastWeek = today.subtract(const Duration(days: 7));
+    Map<String, double> weeklyData = {};
+
+    for (int i = 0; i < stats.length; i++) {
+      DateTime date = DateTime.parse(stats[i]["date"]);
+      if (date.isAfter(lastWeek)) {
+        String dateString = stats[i]["date"].split("-").last;
+        if (weeklyData.containsKey(dateString)) {
+          weeklyData[dateString] =
+              (weeklyData[dateString]! + (stats[i]["time"] / 3600));
+        } else {
+          weeklyData[dateString] = stats[i]["time"] / 3600;
+        }
+      }
+    }
+    for (int i = 1; i <= 7; i++) {
+      DateTime date = lastWeek.add(Duration(days: i));
+      String dateString = date.toString().substring(0, 10).split("-").last;
+      if (!weeklyData.containsKey(dateString)) {
+        weeklyData[dateString] = 0;
+      }
+    }
+    weeklyData = Map.fromEntries(weeklyData.entries.toList()
+      ..sort((e1, e2) => e1.key.compareTo(e2.key)));
+    return weeklyData;
+  }
+
+  List<ColumnSeries<ChartData, String>> getWeeklySeries() {
+    return <ColumnSeries<ChartData, String>>[
+      ColumnSeries<ChartData, String>(
+        dataSource: getChartDataList(),
+        xValueMapper: (ChartData sales, _) => sales.x.toString(),
+        yValueMapper: (ChartData sales, _) => sales.y,
+        dataLabelSettings: const DataLabelSettings(
+            isVisible: false, textStyle: TextStyle(fontSize: 10)),
+      )
+    ];
+  }
+
+  List<ChartData> getChartDataList() {
+    List<ChartData> chartDataList = [];
+    for (var key in aggregatedWeeklyData().keys) {
+      chartDataList.add(ChartData(x: key, y: aggregatedWeeklyData()[key]));
+    }
+    return chartDataList;
+  }
+}
+
+class ChartData {
+  /// Holds the datapoint values like x, y, etc.,
+  ChartData({
+    this.x,
+    this.y,
+  });
+
+  /// Holds x value of the datapoint
+  final dynamic x;
+
+  /// Holds y value of the datapoint
+  final double? y;
 }
